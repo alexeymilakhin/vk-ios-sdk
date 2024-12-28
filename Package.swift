dd5ac39eb2eeb6e5324e41fe3ead1f7928829a93
Aleksey Milakhin <alexeymilakhin@gmail.com>
2024-12-28 22:02:10 +0600

    Поддержка SPM

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "VK-iOS-SDK",
+    products: [
+        .library(
+            name: "VK-iOS-SDK",
+            targets: ["VK-iOS-SDK"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "VK-iOS-SDK",
+            resources: [
+                .copy("VkResources"),
+            ],
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("DynamicFramework"),
+                .headerSearchPath("Source"),
+                .headerSearchPath("Source/API"),
+                .headerSearchPath("Source/API/methods"),
+                .headerSearchPath("Source/API/models"),
+                .headerSearchPath("Source/API/upload"),
+                .headerSearchPath("Source/Core"),
+                .headerSearchPath("Source/Image"),
+                .headerSearchPath("Source/Utils"),
+                .headerSearchPath("Source/Views")
+            ]
+        )
+    ]
+)
